a8b504daff8c3d3d075271e34e00d195222dac11
Makibul <142709442+devMakibul@users.noreply.github.com>
2026-03-08 05:16:02 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,16 @@
 function toggleMenu() {
     document.getElementById("mobile-menu").classList.toggle("hidden");
 }
+
+function toggleTheme() {
+    document.body.classList.toggle("dark");
+
+    if (document.body.className == "dark") {
+        document.getElementById("moon-icon").style.display = "none";
+        document.getElementById("sun-icon").style.display = "flex";
+    } else {
+        document.getElementById("moon-icon").style.display = "flex";
+        document.getElementById("sun-icon").style.display = "none";
+    }
+
+}
